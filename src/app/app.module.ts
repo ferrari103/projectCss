811bc76c070d79316835652ad5b2eb712acdceb4
ferrari103 <erkin.t14@gmail.com>
2020-12-08 23:00:48 +0600

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {CalendarComponent, ChunkPipe} from './calendar/calendar.component';
+import {FormsModule} from '@angular/forms';
+import { CreateEventComponent } from './create-event/create-event.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    ChunkPipe,
+    CreateEventComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
